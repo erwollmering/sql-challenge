fdc244766c8e8f7996c68207f025527503f610d8
erwollmering <erwollmering@gmail.com>
2025-02-25 21:14:03 -0600

    fininshed remaining questions. updated select statement for dept_manager table, updated ERD.

--- EmployeeSQL/HomeworkModule9.sql
+++ EmployeeSQL/HomeworkModule9.sql
@@ -60,7 +60,7 @@ CREATE TABLE dept_manager (
 );
 
 SELECT * 
-FROM dept_emp;
+FROM dept_manager;
 
 ALTER TABLE employees
 ADD CONSTRAINT fk_emp_title FOREIGN KEY (emp_title_id) REFERENCES titles(title_id);
@@ -68,19 +68,70 @@ ADD CONSTRAINT fk_emp_title FOREIGN KEY (emp_title_id) REFERENCES titles(title_i
 SELECT * 
 FROM employees;
 
---List the employee number, last name, first name, sex, and salary of each employee (2 points)
+--1. List the employee number, last name, first name, sex, and salary of each employee (2 points)
 SELECT employees.emp_no, last_name, first_name, sex, salary
 FROM employees
 INNER JOIN salaries--inner join emmployees and salaries tables 
 ON employees.emp_no = salaries.emp_no;
 
---List the first name, last name, and hire date for the employees who were hired in 1986.
+--2. List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT last_name, first_name, hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
---List the manager of each department along with their department number, department name, employee number, last name, and first name.
-SELECT employees.emp_no, dept_emp.emp_no, dept_manager.emp_no
-FROM employees 
-INNER JOIN emp_no
-ON employees.emp_no = dept_emp.emp_no = dept_manager.emp_no;
+--3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT dept_manager.dept_no, dept_name, dept_manager.emp_no, last_name, first_name
+FROM dept_manager 
+INNER JOIN departments
+ON dept_manager.dept_no = departments.dept_no
+INNER JOIN employees
+ON  dept_manager.emp_no = employees.emp_no;
+
+--4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+SELECT dept_emp.dept_no, dept_emp.emp_no, last_name, first_name, dept_name
+FROM dept_emp
+LEFT JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+LEFT JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+ORDER BY last_name;
+
+--5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE '%B';
+
+--6. List each employee in the Sales department, including their employee number, last name, and first name (2 points)
+SELECT dept_no, employees.emp_no, first_name, last_name
+FROM dept_emp
+INNER JOIN employees
+ON dept_emp.emp_no =employees.emp_no
+WHERE dept_no = 'd007';
+
+--7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name (4 points)
+SELECT departments.dept_no, employees.emp_no, first_name, last_name
+FROM dept_emp
+INNER JOIN employees
+ON dept_emp.emp_no =employees.emp_no
+FULL JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_no = 'd007'
+OR departments.dept_no = 'd005';
+
+--8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name) (4 points)
+
+SELECT last_name, COUNT (last_name) AS freq_count_lastname
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count_lastname DESC;
+
+
+
+
+
+
+
+
+
